06a186f4adf77fada0580ea0c84696f449286a40
seculoper <mir8725@naver.com>
2021-09-27 00:05:50 +0900

    Initial commit

---
+++ ping/Test.java
@@ -0,0 +1,50 @@
+package ping;
+
+import robocode.*;
+
+public class Test extends Robot {
+    /**
+     * 봇의 행동을 작성
+     */
+    public void run() {
+        // Initialization of the robot should be put here
+
+        // After trying out your robot, try uncommenting the import at the top,
+        // and the next line:
+
+        // setColors(Color.red,Color.blue,Color.green); // body,gun,radar
+
+        // Robot main loop
+        while (true) {
+            // Replace the next 4 lines with any behavior you would like
+            ahead(100);
+            turnGunRight(360);
+            back(100);
+            turnGunRight(360);
+        }
+    }
+
+    /**
+     * Same with Ray Casting.
+     */
+    public void onScannedRobot(ScannedRobotEvent e) {
+        // Replace the next line with any behavior you would like
+        fire(1);
+    }
+
+    /**
+     * Same with Trigger, Collision.
+     */
+    public void onHitByBullet(HitByBulletEvent e) {
+        // Replace the next line with any behavior you would like
+        back(10);
+    }
+
+    /**
+     * Same above.
+     */
+    public void onHitWall(HitWallEvent e) {
+        // Replace the next line with any behavior you would like
+        back(20);
+    }
+}
